07d654f084977d6e66b8491b17d0c355b8798081
CrisEspi <crisespinosa91@gmail.com>
2021-12-29 12:04:02 -0300

    actualizaci[on 2912

--- js/script.js
+++ js/script.js
@@ -1,33 +1,15 @@
-window.onscroll = function(){
-    scroll = document.documentElement.scrollTop;
+$(document).ready(function(){
 
-    header = document.getElementById("header");
+    $(window).scroll(function (){
+        parallax();
+      })
+      
+      function parallax() {
+          var wScroll = $(window).scrollTop(); // anota que tan lejos de el top estamos
+          $('.parallax').css('background-position', '0 '+(wScroll*0.10)+'px')
+      }
 
-    if (scroll > 20){
-        header.classList.add("nav_mod");
-    }else if(scroll < 20) {
-        header.classList.remove("nav_mod");
-    }
-}
-
-document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
-
-
-menu = document.getElementById("header")
-nav = document.getElementById("nav")
-
-
-function mostrar_menu(){
+      
     
-    menu.classList.toggle("move_content");
-    nav.classList.toggle("move_nav");
-
-}
-
-window.addEventListener("resize", function(){
-
-    if(window.innerWidth > 760){
-        menu.classList.remove("move_content");
-        nav.classList.remove("move_nav");
-    }
 })
+
